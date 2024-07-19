5f24b91563e559a087b3dad11b270f1c75619f54
Marko 3 <163332677+Dauphinsss@users.noreply.github.com>
2024-07-18 21:29:33 -0400

    Update prioridad.c

--- prioridad.c
+++ prioridad.c
@@ -1,29 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
-#include <unistd.h>
 
 #define MAX_PROCESSES 10
 
-// Estructura para almacenar la información de un proceso
 typedef struct {
     int id;
     int priority;
 } Process;
 
-// Función para crear un nuevo proceso
-Process createProcess(int id, int priority) {
-    Process process;
-    process.id = id;
-    process.priority = priority;
-    return process;
+Process processes[MAX_PROCESSES];
+int processCount = 0;
+
+void addProcess(int id, int priority) {
+    if (processCount < MAX_PROCESSES) {
+        processes[processCount].id = id;
+        processes[processCount].priority = priority;
+        processCount++;
+    } else {
+        printf("Process list is full\n");
+    }
 }
 
-// Función para ordenar los procesos por prioridad (menor valor de prioridad primero)
-void sortProcesses(Process processes[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        for (int j = 0; j < n - i - 1; j++) {
-            if (processes[j].priority > processes[j + 1].priority) {
+void sortProcessesByPriority() {
+    for (int i = 0; i < processCount - 1; i++) {
+        for (int j = 0; j < processCount - i - 1; j++) {
+            if (processes[j].priority < processes[j + 1].priority) {
                 Process temp = processes[j];
                 processes[j] = processes[j + 1];
                 processes[j + 1] = temp;
@@ -32,33 +33,22 @@ void sortProcesses(Process processes[], int n) {
     }
 }
 
-// Función que simula la ejecución de un proceso
-void* executeProcess(void* arg) {
-    Process* process = (Process*)arg;
-    printf("Ejecutando proceso %d con prioridad %d\n", process->id, process->priority);
-    sleep(1); // Simula el tiempo de ejecución del proceso
-    pthread_exit(NULL);
+void executeProcesses() {
+    sortProcessesByPriority();
+    for (int i = 0; i < processCount; i++) {
+        printf("Executing process with ID: %d and priority: %d\n", processes[i].id, processes[i].priority);
+    }
 }
 
 int main() {
-    pthread_t threads[MAX_PROCESSES];
-    Process processes[MAX_PROCESSES];
+    addProcess(1, 3);
+    addProcess(2, 1);
+    addProcess(3, 2);
+    addProcess(4, 5);
+    addProcess(5, 4);
 
-    // Creación de procesos con prioridades aleatorias
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        int priority = rand() % 10 + 1; // Prioridad aleatoria entre 1 y 10
-        processes[i] = createProcess(i, priority);
-        printf("Proceso %d con prioridad %d creado\n", processes[i].id, processes[i].priority);
-    }
-
-    // Ordenar los procesos por prioridad
-    sortProcesses(processes, MAX_PROCESSES);
-
-    // Ejecución de procesos en orden de prioridad
-    for (int i = 0; i < MAX_PROCESSES; i++) {
-        pthread_create(&threads[i], NULL, executeProcess, (void*)&processes[i]);
-        pthread_join(threads[i], NULL);
-    }
+    printf("Executing processes in order of priority:\n");
+    executeProcesses();
 
     return 0;
 }
