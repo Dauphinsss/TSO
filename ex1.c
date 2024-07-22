8e70bcb8521a563a746b9c8c9900e4ca5c10fb36
Marko 3 <163332677+Dauphinsss@users.noreply.github.com>
2024-07-22 09:02:25 -0400

    Create ex1.c

---
+++ ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int main(){
+    pid_t pid;
+    int status;
+    for(int i = 0; i < 5; i++){
+        pid = fork()
+        if(pid < 0){
+            exit(1);
+        }else if(pid == 0){
+            sleep(2);
+            printf("soy el proceso hijo %d\n",getpid());
+        }else{
+            wait(&status);
+            printf("el proceso hijo %d ha finalizado",pid);
+        }
+    }
+    return 0;
+}
