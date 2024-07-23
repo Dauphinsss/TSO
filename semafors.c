4e7f761f359536839fc8a1cfb691a5ec0dbf911c
Marko <marcosvelasquezvela123@gmail.com>
2024-07-23 17:59:05 -0400

    f

--- semafors.c
+++ semafors.c
@@ -1,7 +1,7 @@
 //gcc prod_cons.c -pthread
 #include <stdio.h>
 #include <stdlib.h>
-#include <pthread.h>
+#include <minix/mthread.h>
 #include <semaphore.h>
 
 #define TAM 10
@@ -16,8 +16,8 @@ sem_t sem_vacio;
 //sem_lleno se inicializa a 0 indicando que no hay elementos en el buffer.
 sem_t sem_lleno;
 
-//mutex se inicializa para proteger el acceso al buffer.
-pthread_mutex_t mutex; 
+//mutex se inicializa para proteger el acceso al buffer.mthread._mutex_t
+mthread_mutex_t mutex; 
 
 // funcion productor
 void* productor(void* param) {
@@ -29,12 +29,12 @@ void* productor(void* param) {
 	//Espera hasta que haya un espacio vacio en el buffer 
         sem_wait(&sem_vacio); 
 	// Bloquea el acceso al buffer
-        pthread_mutex_lock(&mutex); 
+        mthread_mutex_lock(&mutex); 
 	// Anade el elemento al buffer y aumenta el contador
         buffer[count++] = item; 
         printf("Produce: %d\n", item);
 	// Desbloquea el buffer 
-        pthread_mutex_unlock(&mutex); 
+        mthread_mutex_unlock(&mutex); 
 	// Senala que hay un nuevo elemento en el buffer
         sem_post(&sem_lleno); 
     }
@@ -47,12 +47,12 @@ void* consumidor(void* arg) {
         // Espera hasta que haya al menos un elemento en el buffer
 	sem_wait(&sem_lleno); 
 	// Bloquea el acceso al buffer
-        pthread_mutex_lock(&mutex); 
+        mthread_mutex_lock(&mutex); 
 	// Extrae el elemento del buffer y decrementa el contador.
         item = buffer[--count]; 
         printf("Consume: %d\n", item);
 	// Desbloquea el buffer
-        pthread_mutex_unlock(&mutex); 
+        mthread_mutex_unlock(&mutex); 
 	// Senala que hay un nuevo espacio vacío en el buffer 
         sem_post(&sem_vacio); 
     }	
@@ -60,25 +60,25 @@ void* consumidor(void* arg) {
 }
 
 int main() {
-    pthread_t pp, pc;
+    mthread_t pp, pc;
 
     // inicializar semaforos
     sem_init(&sem_vacio, 0, TAM);
     sem_init(&sem_lleno, 0, 0);
-    pthread_mutex_init(&mutex, NULL);
+    mthread_mutex_init(&mutex, NULL);
 
     
-    pthread_create(&pp, NULL, productor, NULL);
-    pthread_create(&pc, NULL, consumidor, NULL);
+    mthread_create(&pp, NULL, productor, NULL);
+    mthread_create(&pc, NULL, consumidor, NULL);
 
 
-    pthread_join(pp, NULL);
-    pthread_join(pc, NULL);
+    mthread_join(pp, NULL);
+    mthread_join(pc, NULL);
 
     // Clean up
     sem_destroy(&sem_vacio);
     sem_destroy(&sem_lleno);
-    pthread_mutex_destroy(&mutex);
+    mthread_mutex_destroy(&mutex);
 
     return 0;
 }
