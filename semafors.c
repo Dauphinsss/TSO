88d8bd793f53a487b49a3567c329cb6ff899a21f
Marko <marcosvelasquezvela123@gmail.com>
2024-07-23 17:33:33 -0400

    fasf

--- semafors.c
+++ semafors.c
@@ -1,62 +1,83 @@
+//gcc prod_cons.c -pthread
 #include <stdio.h>
 #include <stdlib.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <unistd.h>
 
-#define TAMANO_BUFFER 20
+#define TAM 10
 
-int buffer[TAMANO_BUFFER];
-int contador = 0;
+int buffer[TAM];
+int count = 0;
 
-sem_t vacio;
-sem_t lleno;
-pthread_mutex_t mutex;
+// se inicializa indicando que el buffer está completamente vacío al principio.
 
-void *productor(void *param) {
+sem_t sem_vacio; 
+
+//sem_lleno se inicializa a 0 indicando que no hay elementos en el buffer.
+sem_t sem_lleno;
+
+//mutex se inicializa para proteger el acceso al buffer.
+pthread_mutex_t mutex; 
+
+// funcion productor
+void* productor(void* param) {
     int item;
-    while (1) {
-        item = rand() % 100;
-        sleep(1);
+    for (int i = 0; i < 20; ++i) {
+	//Produce un elemento aleatorio.
+        item = rand() % 100; 
 
-        sem_wait(&vacio);
-        pthread_mutex_lock(&mutex);
-        buffer[contador++] = item;
-        printf("Producido: %d\n", item);
-        pthread_mutex_unlock(&mutex);
-        sem_post(&lleno);
+	//Espera hasta que haya un espacio vacio en el buffer 
+        sem_wait(&sem_vacio); 
+	// Bloquea el acceso al buffer
+        pthread_mutex_lock(&mutex); 
+	// Anade el elemento al buffer y aumenta el contador
+        buffer[count++] = item; 
+        printf("Produce: %d\n", item);
+	// Desbloquea el buffer 
+        pthread_mutex_unlock(&mutex); 
+	// Senala que hay un nuevo elemento en el buffer
+        sem_post(&sem_lleno); 
     }
+    return NULL;
 }
 
-void *consumidor(void *param) {
+void* consumidor(void* arg) {
     int item;
-    while (1) {
-        sem_wait(&lleno);
-        pthread_mutex_lock(&mutex);
-
-        item = buffer[--contador];
-        printf("Consumido: %d\n", item);
-
-        pthread_mutex_unlock(&mutex);
-        sem_post(&vacio);
-
-        sleep(2);
-    }
+    for (int i = 0; i < 20; ++i) {
+        // Espera hasta que haya al menos un elemento en el buffer
+	sem_wait(&sem_lleno); 
+	// Bloquea el acceso al buffer
+        pthread_mutex_lock(&mutex); 
+	// Extrae el elemento del buffer y decrementa el contador.
+        item = buffer[--count]; 
+        printf("Consume: %d\n", item);
+	// Desbloquea el buffer
+        pthread_mutex_unlock(&mutex); 
+	// Senala que hay un nuevo espacio vacío en el buffer 
+        sem_post(&sem_vacio); 
+    }	
+    return NULL;
 }
 
 int main() {
-    pthread_t hilo_productor, hilo_consumidor;
+    pthread_t pp, pc;
 
-    sem_init(&vacio, 0, TAMANO_BUFFER);
-    sem_init(&lleno, 0, 0);
+    // inicializar semaforos
+    sem_init(&sem_vacio, 0, TAM);
+    sem_init(&sem_lleno, 0, 0);
     pthread_mutex_init(&mutex, NULL);
-    pthread_create(&hilo_productor, NULL, productor, NULL);
-    pthread_create(&hilo_consumidor, NULL, consumidor, NULL);
-    pthread_join(hilo_productor, NULL);
-    pthread_join(hilo_consumidor, NULL);
 
-    sem_destroy(&vacio);
-    sem_destroy(&lleno);
+    
+    pthread_create(&pp, NULL, productor, NULL);
+    pthread_create(&pc, NULL, consumidor, NULL);
+
+
+    pthread_join(pp, NULL);
+    pthread_join(pc, NULL);
+
+    // Clean up
+    sem_destroy(&sem_vacio);
+    sem_destroy(&sem_lleno);
     pthread_mutex_destroy(&mutex);
 
     return 0;
