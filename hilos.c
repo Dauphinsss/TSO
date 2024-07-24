98780200cd631763c04d92d7379867f36beca6ac
Marko 3 <163332677+Dauphinsss@users.noreply.github.com>
2024-07-24 12:06:26 -0400

    Update hilos.c

--- hilos.c
+++ hilos.c
@@ -1,8 +1,40 @@
-#include <minix/mthread.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
 
-int main(int argc, char const *argv[])
-{
-    /* code */
+// Función que será ejecutada por cada hilo
+void* print_message_function(void* ptr) {
+    char* message = (char*) ptr;
+    printf("%s\n", message);
+    return NULL;
+}
+
+int main() {
+    pthread_t thread1, thread2;
+    const char* message1 = "Hola desde el hilo 1";
+    const char* message2 = "Hola desde el hilo 2";
+
+    // Crear hilos
+    if (pthread_create(&thread1, NULL, print_message_function, (void*) message1)) {
+        fprintf(stderr, "Error creando el hilo 1\n");
+        return 1;
+    }
+
+    if (pthread_create(&thread2, NULL, print_message_function, (void*) message2)) {
+        fprintf(stderr, "Error creando el hilo 2\n");
+        return 1;
+    }
+
+    // Esperar a que los hilos terminen
+    if (pthread_join(thread1, NULL)) {
+        fprintf(stderr, "Error uniendo el hilo 1\n");
+        return 2;
+    }
+
+    if (pthread_join(thread2, NULL)) {
+        fprintf(stderr, "Error uniendo el hilo 2\n");
+        return 2;
+    }
+
     return 0;
 }
